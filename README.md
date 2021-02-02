3748366a4a6856be6fae6c073746e9add4ba4272
yoshihiro.yoshida <marchwind0079@gmail.com>
2021-02-03 03:15:17 +0900

    feat: add 0002

--- README.md
+++ README.md
@@ -2,11 +2,16 @@
 
 https://leetcode.com/problemset/algorithms/
 
+
+## Solution
+Discuss -> Most Votes
+
+## initial setting
 ```
 $ export PIPENV_VENV_IN_PROJECT=true
 $ pipenv --python 3                   # .venvディレクトリに仮想環境が生成される
 $ pipenv install
 $ pipenv install numpy                # numpyをインストールする例です
-```
 
 https://qiita.com/y-tsutsu/items/54c10e0b2c6b565c887a
+```
---
+++ algorithms/0002_add_two_numbers.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    result = solution.addTwoNumbers([2, 4, 3], [5, 6, 4])
+    print(result)
